ce86ac208473c5dfb644df22bb11ed6defa2102b
abdiaelema76 <gababgoabdiaelema@gmail.com>
2023-12-03 09:51:48 +0300

    Pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A function that creates a pascal triangle
+"""
+def pascal_triangle(n):
+    """
+     a function that returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+    #The First row
+    triangle = [[1]]
+
+    #generating subsequent rows
+    for i in range(1, n):
+        row = [1] #each row in pascal triangle starts with 1
+        #computing the values between the first and last 1's
+        for j in range (1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1[j]])
+            
+        row.append(1) #each row in pascal triangle ends with 1
+        triangle.append(row)
+    return triangle  # noqa: F706
